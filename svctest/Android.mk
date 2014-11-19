b3d9ab7b37e07efa143cb72cf5ca30e9d724dd13
Roy20141119 <rosalie_2006@163.com>
2014-11-19 11:15:18 +0800

    modified better.

--- svctest/Android.mk
+++ svctest/Android.mk
@@ -3,5 +3,5 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES:=test.cpp
 LOCAL_SHARED_LIBRARIES:=libZPClient
 LOCAL_MODULE_TAGS:=optional
-LOCAL_MODULE:=svctest
+LOCAL_MODULE:=zpclient
 include $(BUILD_EXECUTABLE)
